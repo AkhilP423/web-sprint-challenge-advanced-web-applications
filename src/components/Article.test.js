68865eb1ee321559f16849c034a2fbaf3a5613ad
Akhil Pinnani <akhilp@Akhils-MacBook-Pro.local>
2021-12-17 16:00:05 -0800

    finished testing page, cant figure one test out, will work until time runs out.

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,22 +1,57 @@
 import React from 'react';
-import '@testing-library/jest-dom';
-
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
 import Article from './Article';
 
-test('renders component without errors', ()=> {
+//test article, given in challenge
+const TestArticle = {
+
+    id: 'aMqwd', //unique article id
+    headline: "headline", //title of article
+    createdOn: '2021-08-09T18:02:38-04:00 ', //timestamp of when article was added
+    summary: "summary", //short summary statement of article
+    body: ""  //paragraph of article text
+    
+}
+//cannot figure this out!!!!! Error lies in Article.js for some reason
+test('renders component without errors', () => {
+    render(<Article />);
 });
 
-// test('renders headline, author from the article when passed in through props', ()=> {
-// });
+//headline render working when passed thru props
+test('renders headline, author from the article when passed in through props', () => {
 
-// test('renders "Associated Press" when no author is given', ()=> {
-// });
+    render(<Article article={TestArticle}/>);
+    
+    const headline = screen.queryByTestId('headline');
+    const author = screen.queryByTestId('author');
+ 
+    expect(headline).toBeInTheDocument();
+    expect(author).toBeInTheDocument();
 
-// test('executes handleDelete when the delete button is pressed', ()=> {
-// });
+});
+//renders AP by default
+test('renders "Associated Press" when no author is given', () => {
+
+    render(<Article article={TestArticle} />);
+    const author = screen.queryByTestId('author');
+    expect(author).toHaveTextContent(/Associated Press/i);
+
+});
+//handledelete working :)
+test('executes handleDelete when the delete button is pressed', () => {
+
+    const handleDelete = jest.fn();
+    render(<Article article={TestArticle} handleDelete={handleDelete}/>);
+    const deleteBtn = screen.queryByTestId('deleteButton');
+    userEvent.click(deleteBtn);
+    expect(handleDelete).toHaveBeenCalled();
+
+});
+
 
 //Task List:
-//1. Complete all above tests. Create test article data when needed.
+//1. Complete all above tests. Create test article data when needed. DOne
